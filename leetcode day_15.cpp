cc068de6bd13958fd79f7384306fb6db129b1e6f
Piyush Regar <piyushdadwadiya@gmail.com>
2025-01-16 12:35:00 +0530

    Create leetcode day_15.cpp

---
+++ leetcode day_15.cpp
@@ -0,0 +1,31 @@
+/*
+ * LeetCode Problem: Minimize XOR
+ * Problem Number: 2429
+ * Difficulty: [ Medium ]
+ * Problem Link: https://leetcode.com/problems/minimize-xor/
+ */
+
+
+//Leetcode solution
+
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int count = __builtin_popcount(num2);
+
+        int res = 0;
+        for(int i=31; i>=0 and count; i--){
+            if(num1 & (1<<i)){
+                count--;
+                res += (1<<i);
+            }
+        }
+        for(int i=0; i<32 and count; i++) {
+            if((num1 & (1<<i))==0) {
+                count--;
+                res += (1<<i);
+            }
+        }
+        return res;
+    }
+};
